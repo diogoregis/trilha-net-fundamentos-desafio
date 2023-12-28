94358432ae16a05bbf9eff59f3775c05f4d75355
Diogo Regis <diogo.regis.silva@gmail.com>
2023-12-28 01:00:13 -0300

    refatorando class Program, alterando menu

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,6 +1,5 @@
 ﻿using DesafioFundamentos.Models;
 
-// Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
@@ -13,13 +12,11 @@ precoInicial = Convert.ToDecimal(Console.ReadLine());
 Console.WriteLine("Agora digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
 
-// Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
@@ -27,7 +24,7 @@ while (exibirMenu)
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("0 - Encerrar");
 
     switch (Console.ReadLine())
     {
@@ -43,7 +40,7 @@ while (exibirMenu)
             es.ListarVeiculos();
             break;
 
-        case "4":
+        case "0":
             exibirMenu = false;
             break;
 
@@ -56,4 +53,4 @@ while (exibirMenu)
     Console.ReadLine();
 }
 
-Console.WriteLine("O programa se encerrou");
+Console.WriteLine("Encerrando programa.");
