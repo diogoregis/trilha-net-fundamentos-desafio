7978cafac05b4f405781a3acde91bce40b5fd298
Diogo Regis <diogo.regis.silva@gmail.com>
2023-12-28 00:27:31 -0300

    implementando o metodo removerVeiculo, na class Estacionamento

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -22,27 +22,16 @@ public class Estacionamento
 
     public void RemoverVeiculo()
     {
-        Console.WriteLine("Digite a placa do veículo para remover:");
+        Console.WriteLine("Digite a placa do veículo para check-out:");
+        string placa = Console.ReadLine();
 
-        // Pedir para o usuário digitar a placa e armazenar na variável placa
-        // *IMPLEMENTE AQUI*
-        string placa = "";
-
-        // Verifica se o veículo existe
         if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
         {
             Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-            // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-            // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-            // *IMPLEMENTE AQUI*
-            int horas = 0;
-            decimal valorTotal = 0; 
-
-            // TODO: Remover a placa digitada da lista de veículos
-            // *IMPLEMENTE AQUI*
-
-            Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            int horas = Convert.ToInt32(Console.ReadLine());
+            decimal valorTotal = precoPorHora * horas + precoInicial; 
+            veiculos.Remove(placa);
+            Console.WriteLine($"O veículo {placa} foi removido. Preço total pago: {valorTotal:C}");
         }
         else
         {
